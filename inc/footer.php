3b79f6d03b14b04a638eacb5d44f94537658c892
Ryan Teller <rayane68100@gmail.com>
2018-11-02 23:18:19 +0100

    Ajout du popup d'information des cookies

--- inc/footer.php
+++ inc/footer.php
@@ -5,4 +5,26 @@
         <a href="index.php">Accueil</a><a href="#">Support</a><a href="#">Terms of Service</a><a href="#">Legal</a><a href="#">Help</a><a href="logout.php">Se déconnecter</a>
     </div>
 </footer>
+<link rel="stylesheet" type="text/css" href="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.css" />
+<script src="//cdnjs.cloudflare.com/ajax/libs/cookieconsent2/3.1.0/cookieconsent.min.js"></script>
+<script>
+window.addEventListener("load", function(){
+window.cookieconsent.initialise({
+  "palette": {
+    "popup": {
+      "background": "#eb6c44",
+      "text": "#ffffff"
+    },
+    "button": {
+      "background": "#f5d948"
+    }
+  },
+  "theme": "edgeless",
+  "content": {
+    "message": "Ce site utilise des cookies pour vous garantir la meilleure expérience sur notre site.",
+    "dismiss": "J'ai compris",
+    "link": "En apprendre plus"
+  }
+})});
+</script>
 <!-- Footer End -->
